3148e7df748106c1691a1f554cdd0232864b2d1d
414 <414@DESKTOP-SQ45H3U>
2018-07-02 16:10:14 +0900

    first commit

---
+++ src/HelloJava.java
@@ -0,0 +1,13 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HelloJava");
+		System.out.println("20171422");
+		System.out.println("È²ÁöÈñ");
+		
+	}
+
+}
+    
