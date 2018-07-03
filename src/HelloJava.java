3596a17dc6e360432711a9961d75218e74a52112
414 <414@DESKTOP-SQ45H3U>
2018-07-03 17:05:20 +0900

    난수생성

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -4,8 +4,10 @@ public class HelloJava {
 	public static void main(String[] args) {
 		
 		System.out.println("HelloJava");
-		System.out.println("20171422");
-		System.out.println("È²ÁöÈñ");
+		System.out.println("HelloJ");
+		
+		
+		
 		
 	}
 
--- src/my/day02/NumberGame.java
+++ src/my/day02/NumberGame.java
@@ -1,9 +1,16 @@
 package my.day02;
-
+import java.util.*;
 public class NumberGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int num = rand.nextInt(100);
+		System.out.println("컴퓨터가 생성한 난수 : " + num);
+		
+		
+		
+		
+		
 
 	}
 
